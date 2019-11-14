0c4d8b119e7e70f0c8dcad42ebdc4b3f83efab9a
unknown <gvoytseshko@ufl.edu>
2019-11-14 17:35:56 -0500

    modified main.py to get axp stuff

--- main.py
+++ main.py
@@ -1,126 +1,97 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-
+from flask import Flask
 from google.cloud import firestore
 import requests
-from flask import Flask
+import json
 
-db = firestore.Client.from_service_account_json('./pinterestservice-firebase-adminsdk-6ubsp-9e03db19db.json')
 app = Flask(__name__)
+db = firestore.Client.from_service_account_json('cisproject2-firebase-adminsdk-p3dru-ffd0d7e5ed.json')
 
+def get_price():
+	try:
+		response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
+			params={'symbols': 'AXP,VXX190517P00016000', 'greeks': 'false'},
+			headers={'Authorization': 'Bearer n7UzJWyS028Oi8PianpcNseKBI6j', 'Accept': 'application/json'}
+		)
+	except requests.exceptions.RequestException as e:
+	  return('Exception during request' + str(e))
+	else:
+		json_response = response.json()
+		price = json_response["quotes"]["quote"]["last"]
+		return price
 
-def getUserHoldingsDb(userID):
-    doc_ref = db.collection(u'UserHoldings_AXP').document(userID)
-    try:
-        doc = doc_ref.get()
-        return  {"stock_amnt" :(doc.to_dict()['stock_amnt'])}
-    except TypeError:
-        return 0
+def user_holdings(userID):
+	ref = db.collection('user-shares').document(userID)
+	try:
+		doc = ref.get()
+		return {"shares": (doc.to_dict()['shares'])}
+	except:
+		return 'Error.'
 
-def updateUserHoldingsDb(userID, amnt, action):
-    currStocks = getUserHoldingsDb(userID)['stock_amnt']
-    
-    if action == "buy":
-        updatedStocks = currStocks + amnt
-    else:
-        updatedStocks = currStocks - amnt
- 
-    doc_ref = db.collection(u'UserHoldings_AXP').document(userID)
-    doc_ref = doc_ref.set({
-            u'stock_amnt': updatedStocks
-            })
-    return 1
+def obs_holdings():
+	ref = db.collection('obs-holding').document("obs-uber")
+	try:
+		doc = ref.get()
+		return {"shares": (doc.to_dict()['shares'])}
+	except:
+		return 'Error'
 
-def obsGetAvailableStocks():
-    doc_ref = db.collection(u'OBS_holdings').document('FGABKyqIk71Fn8e1n94j')
-    try:
-        doc = doc_ref.get()
-        return  {
-                "total_stocks": doc.to_dict()['total_stocks'],
-                "cost_to_bank": doc.to_dict()['cost_to_bank']     
-                 }
-    except TypeError:
-        return 0
-   
-def obsUpdateStockHoldings(amount, action):
-    # add or sell additional stocks
-    cost = amount * getPriceFromTradier()
-    currCostToBank = obsGetAvailableStocks()['cost_to_bank']
-    currStocks = obsGetAvailableStocks()['total_stocks']
+def update_obs_holdings(amount):
+	ref = db.collection('obs-holding').document("obs-uber")
+	try:
+		ref.set({
+			u"shares": amount
+		})
+	except:
+		return False
 
-    if action == "buy":
-        updatedCost = currCostToBank - cost
-        updatedStocks = currStocks + amount
-    else:
-        updatedCost = currCostToBank + cost
-        updatedStocks = currStocks - amount            
-    
-    doc_ref = db.collection(u'OBS_holdings').document('FGABKyqIk71Fn8e1n94j')
-    doc_ref = doc_ref.set({
-            u'total_stocks': updatedStocks,
-            u'cost_to_bank': updatedCost
-            })
-            
-    return 1
+def updates_user_holdings(userID, amount):
+	ref = db.collection('user-shares').document(userID)
+	current_holdings = ref.get().to_dict()['shares']
+	try:
+		ref.set({
+			u"shares": (current_holdings + amount)
+		})
+	except:
+		print("Error occured during user stock purchasing.")
 
-def getPriceFromTradier():
-    try:
-        response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
-                                params={'symbols': 'AXP,VXX190517P00016000', 'greeks': 'false'},
-                                headers={'Authorization': 'Bearer uZOWMJ0Hh2jcz2g9GE2Xa9YNPhMC', 'Accept': 'application/json'})
-    except requests.exceptions.RequestException as e:
-        print("Get stock price request error: ", e)
-        
-    resp_json = response.json()
-    lastPrice = resp_json["quotes"]["quote"]["last"]
-    return lastPrice
-
-
-
-@app.route("/",) 
+@app.route("/",)
 def index():
-    return "Welcome to Python Server"
+	return "OBS - American Express Microservice"
 
 @app.route("/getStockPrice",)
-def getStockPrice():
-    result = {"stock_price": getPriceFromTradier()}
-    return result
+def price():
+	return {"axp_price": get_price()}
 
-@app.route("/getUserHoldings/<userID>",) 
-def getUser(userID):
-    result = {"stock_amnt": getUserHoldingsDb(userID)}
-    return result
+@app.route("/getUserHoldings/<userID>",)
+def userHoldings(userID):
+	return {"user_shares": user_holdings(userID)['shares']}
 
-@app.route("/getOBSHoldings",) 
-def getOBS():
-    result = {"stock_amnt": obsGetAvailableStocks()}
-    return result
+@app.route("/getOBSHoldings",)
+def obsHoldings():
+	return {"obs_shares": obs_holdings()['shares']}
 
 @app.route("/buy/<userID>/<int:amount>")
-def buyStocks(userID, amount):
-    #check bank holdings
-    bankholdings = obsGetAvailableStocks()['total_stocks']
-    if amount > bankholdings:
-        amntNeeded = amount - bankholdings 
-        obsUpdateStockHoldings((amntNeeded + 1), "buy")
-    # bank sells stocks
-    obsUpdateStockHoldings(amount, "sell")
-    # user buys stock
-    updateUserHoldingsDb(userID, amount, "buy")
-    return "bought stocks"
+def buy(userID,amount):
+	obs_position = obs_holdings()['shares']
+	if amount >= obs_position:
+		update_obs_holdings(5000)
+	else:
+		update_obs_holdings(obs_position - amount)
+	updates_user_holdings(userID,amount)
+	return {"testing": obs_holdings()['shares']}
 
 @app.route("/sell/<userID>/<int:amount>")
-def sellStocks(userID, amount):    
-    holdings = getUserHoldingsDb(userID)['stock_amnt']
-    if amount > holdings:
-        return "Sorry you do not own enough stocks to sell that amount."
-    # bank buys stock from user
-    obsUpdateStockHoldings(amount,"buy")
-    # user sells stock to bank
-    updateUserHoldingsDb(userID, amount, "sell")
-    return "sold stocks"
+def sell(userID, amount):
+	obs_position = obs_holdings()['shares']
+	user_position = user_holdings(userID)['shares']
+	if amount > user_holdings:
+		return 'Error: insufficient user inventory.'
+	else:
+		return {
+			"user": userID,
+			"shares": amount
+		}
 
-if __name__ == "__main__":
-    app.run(debug=True)
-#
+if __name__ == '__main__':
+	app.run(debug=True)
