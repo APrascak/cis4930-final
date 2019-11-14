015591c7e71e48bfdff3b6ef6bd1d4007e5c5d57
sydneynachinger <32098695+sydneynachinger@users.noreply.github.com>
2019-11-14 15:47:54 -0500

    removed costtobank calculations

--- PINSService/src/main.py
+++ PINSService/src/main.py
@@ -11,17 +11,30 @@ app = Flask(__name__)
 
 
 
+
+def getPriceFromTradier():
+    try:
+        response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
+                                params={'symbols': 'PINS,VXX190517P00016000', 'greeks': 'false'},
+                                headers={'Authorization': 'Bearer uZOWMJ0Hh2jcz2g9GE2Xa9YNPhMC', 'Accept': 'application/json'})
+    except requests.exceptions.RequestException as e:
+        print("Get stock price request error: ", e)
+        
+    resp_json = response.json()
+    lastPrice = resp_json["quotes"]["quote"]["last"]
+    return lastPrice
+
 def getUserHoldingsDb(userID):
     doc_ref = db.collection(u'UserHoldings_PINS').document(userID)
     try:
         doc = doc_ref.get()
         return  {"stock_amnt" :(doc.to_dict()['stock_amnt'])}
     except TypeError:
-        return 0
+        return {"stock_amnt" : 0}
 
 def updateUserHoldingsDb(userID, amnt, action):
+
     currStocks = getUserHoldingsDb(userID)['stock_amnt']
-    
     if action == "buy":
         updatedStocks = currStocks + amnt
     else:
@@ -39,43 +52,25 @@ def obsGetAvailableStocks():
         doc = doc_ref.get()
         return  {
                 "total_stocks": doc.to_dict()['total_stocks'],
-                "cost_to_bank": doc.to_dict()['cost_to_bank']     
                  }
     except TypeError:
         return 0
    
 def obsUpdateStockHoldings(amount, action):
     # add or sell additional stocks
-    cost = amount * getPriceFromTradier()
-    currCostToBank = obsGetAvailableStocks()['cost_to_bank']
     currStocks = obsGetAvailableStocks()['total_stocks']
-
+    print("current ",currStocks)
     if action == "buy":
-        updatedCost = currCostToBank - cost
         updatedStocks = currStocks + amount
     else:
-        updatedCost = currCostToBank + cost
         updatedStocks = currStocks - amount            
     
     doc_ref = db.collection(u'OBS_holdings').document('FGABKyqIk71Fn8e1n94j')
     doc_ref = doc_ref.set({
             u'total_stocks': updatedStocks,
-            u'cost_to_bank': updatedCost
             })
             
     return 1
-
-def getPriceFromTradier():
-    try:
-        response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
-                                params={'symbols': 'PINS,VXX190517P00016000', 'greeks': 'false'},
-                                headers={'Authorization': 'Bearer uZOWMJ0Hh2jcz2g9GE2Xa9YNPhMC', 'Accept': 'application/json'})
-    except requests.exceptions.RequestException as e:
-        print("Get stock price request error: ", e)
-        
-    resp_json = response.json()
-    lastPrice = resp_json["quotes"]["quote"]["last"]
-    return lastPrice
 
 
 
@@ -95,16 +90,17 @@ def getUser(userID):
 
 @app.route("/getOBSHoldings",) 
 def getOBS():
-    result = {"stock_amnt": obsGetAvailableStocks()['total_stocks']}
+    result = {"obs_holdings": obsGetAvailableStocks()['total_stocks']}
     return result
 
 @app.route("/buy/<userID>/<int:amount>")
 def buyStocks(userID, amount):
     #check bank holdings
     bankholdings = obsGetAvailableStocks()['total_stocks']
+    print("bankholdings ", bankholdings)
     if amount > bankholdings:
         amntNeeded = amount - bankholdings 
-        obsUpdateStockHoldings((amntNeeded + 1), "buy")
+        obsUpdateStockHoldings((amntNeeded + 5000), "buy")
     # bank sells stocks
     obsUpdateStockHoldings(amount, "sell")
     # user buys stock
