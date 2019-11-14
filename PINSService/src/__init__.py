9824a7ac969e6a67025e47f8f4e77d6c5e1c31b5
sydneynachinger <32098695+sydneynachinger@users.noreply.github.com>
2019-11-14 13:24:58 -0500

    changed init create app back to normal

--- PINSService/src/__init__.py
+++ PINSService/src/__init__.py
@@ -9,14 +9,8 @@ database = firestore.Client.from_service_account_json('/Users/Sydney/Desktop/cis
 def create_app_colls(test = None):
     flask_app = Flask(__name__)
     flask_app.app_context().push()
-    if test is None:    
-        user_holdings = u'UserHoldings_PINS'
-        obs_holdings = u'OBS_holdings'
-    else:
-        user_holdings = u'Test_UserHoldings_PINS'
-        obs_holdings = u'Test_OBS_holdings'
         
-    return flask_app, user_holdings, obs_holdings
+    return flask_app
 
 
 #
