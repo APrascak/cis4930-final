929a7a568d61580948bfffc17a5c03f5e3774f2c
unknown <gvoytseshko@ufl.edu>
2019-11-14 20:35:26 -0500

    added test cases

---
+++ src/main.py
@@ -0,0 +1,97 @@
+
+from flask import Flask
+from google.cloud import firestore
+import requests
+import json
+
+app = Flask(__name__)
+db = firestore.Client.from_service_account_json('cisproject2-firebase-adminsdk-p3dru-ffd0d7e5ed.json')
+
+def get_price():
+	try:
+		response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
+			params={'symbols': 'AXP,VXX190517P00016000', 'greeks': 'false'},
+			headers={'Authorization': 'Bearer n7UzJWyS028Oi8PianpcNseKBI6j', 'Accept': 'application/json'}
+		)
+	except requests.exceptions.RequestException as e:
+	  return('Exception during request' + str(e))
+	else:
+		json_response = response.json()
+		price = json_response["quotes"]["quote"]["last"]
+		return price
+
+def user_holdings(userID):
+	ref = db.collection('user-shares').document(userID)
+	try:
+		doc = ref.get()
+		return {"shares": (doc.to_dict()['shares'])}
+	except:
+		return 'Error.'
+
+def obs_holdings():
+	ref = db.collection('obs').document("axp")
+	try:
+		doc = ref.get()
+		return {"shares": (doc.to_dict()['shares'])}
+	except:
+		return 'Error'
+
+def update_obs_holdings(amount):
+	ref = db.collection('obs').document("axp")
+	try:
+		ref.set({
+			u"shares": amount
+		})
+	except:
+		return False
+
+def updates_user_holdings(userID, amount):
+	ref = db.collection('user-shares').document(userID)
+	current_holdings = ref.get().to_dict()['shares']
+	try:
+		ref.set({
+			u"shares": (current_holdings + amount)
+		})
+	except:
+		print("Error occured during user stock purchasing.")
+
+@app.route("/",)
+def index():
+	return "OBS - American Express Microservice"
+
+@app.route("/getStockPrice",)
+def price():
+	return {"stock_price": get_price()}
+
+@app.route("/getUserHoldings/<userID>",)
+def userHoldings(userID):
+	return {"user_shares": user_holdings(userID)['shares']}
+
+@app.route("/getOBSHoldings",)
+def obsHoldings():
+	return {"obs_shares": obs_holdings()['shares']}
+
+@app.route("/buy/<userID>/<int:amount>")
+def buy(userID,amount):
+	obs_position = int(obs_holdings()['shares'])
+	if amount >= obs_position:
+		update_obs_holdings(5000)
+	else:
+		update_obs_holdings(obs_position - amount)
+	updates_user_holdings(userID,amount)
+	return {"testing": obs_holdings()['shares']}
+
+@app.route("/sell/<userID>/<int:amount>")
+def sell(userID, amount):
+	obs_position = int(obs_holdings()['shares'])
+	user_position = int(user_holdings(userID)['shares'])
+	if amount > user_holdings:
+		return 'Error: insufficient user inventory.'
+	else:
+		return {
+			"user": userID,
+			"shares": amount
+		}
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ src/test_main.py
@@ -0,0 +1,75 @@
+# tests for Pinterest Stock Service
+# to run tests use terminal, cd into test directory, use command 'py.test'
+import pytest
+from main import app
+import requests
+import json
+from unittest.mock import patch
+
+
+@pytest.fixture(scope='module')
+def test_client(): 
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = app.test_client()
+    
+    # Establish an application context before running the tests.
+    ctx = app.app_context()  
+    ctx.push()
+ 
+    yield testing_client  # this is where the testing happens!
+    ctx.pop()
+    
+
+def getStockPrice():
+    try:
+        response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
+                                params={'symbols': 'AXP,VXX190517P00016000', 'greeks': 'false'},
+                                headers={'Authorization': 'Bearer uZOWMJ0Hh2jcz2g9GE2Xa9YNPhMC', 'Accept': 'application/json'})
+    except requests.exceptions.RequestException as e:
+        print("Get stock price request error: ", e)
+        
+    resp_json = response.json()
+    lastPrice = resp_json["quotes"]["quote"]["last"]
+    return lastPrice
+
+
+def test_home_page(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b"OBS - American Express Microservice" in response.data
+    
+    
+def test_get_stock_price(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/getStockPrice' page is requested (GET)
+    THEN check the response is current stock price from tradier
+    """
+    expectedPrice = getStockPrice()
+    response = test_client.get('/getStockPrice') 
+    
+    assert response.status_code == 200
+    assert expectedPrice == json.loads( response.data )['stock_price']
+     
+@patch('src.main.obs_holdings')
+def test_get_obs_holdings(mock_obsGetAvailableStocks, test_client):
+   
+    mock_obsGetAvailableStocks.return_value = { "shares": 5000 }
+    response = test_client.get('/getOBSHoldings')
+    assert response.status_code == 200
+    assert (json.loads(response.data)['obs_shares']) == 5000
+    
+
+@patch('main.user_holdings')
+def test_get_user_holdings(mock_user_holdings, test_client):
+
+    mock_user_holdings.return_value = { "shares": 10 }
+    response = test_client.get('/getUserHoldings/test')    
+    assert response.status_code == 200
+    assert (json.loads(response.data)["user_shares"]) == 10
