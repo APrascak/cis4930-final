7d70c0d7a7962784749e24b8c7d2055e8725c387
unknown <gvoytseshko@ufl.edu>
2019-11-28 11:58:17 -0500

    Added StockAccount component

--- frontend/src/components/Account/index.js
+++ frontend/src/components/Account/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+import StockAccount from '../StockAcount';
 import { AuthUserContext, withAuthorization } from '../Session';
 const AccountPage = () => (
   <AuthUserContext.Consumer>
@@ -9,6 +10,11 @@ const AccountPage = () => (
         <h1>Account: {authUser.email}</h1>
         <PasswordForgetForm />
         <PasswordChangeForm />
+        <StockAccount data="1"/>
+        <StockAccount data="2"/>
+        <StockAccount data="3"/>
+        <StockAccount data="4"/>
+        <StockAccount data="5"/>
       </div>
     )}
   </AuthUserContext.Consumer>
---
+++ frontend/src/components/StockAcount/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthUserContext, withAuthorization } from '../Session';
+const StockAccountPage = props => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Stock account number: {props.data}</h1>
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(StockAccountPage);
