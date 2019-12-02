e6f865db262737bd1cc0257e1936819704892bfb
Alex Prascak <aprascak@ufl.edu>
2019-12-01 21:41:07 -0500

    Cleaned up registration and dashboard.

--- frontend/src/components/Admin/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from '../Navigation';
-const Admin = () => (
-    <div>
-    <h1>Admin</h1>
-  </div>
-);
-export default Admin;
--- frontend/src/components/Landing/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from '../Navigation';
-const Landing = () => (
-    <div>
-    <h1>Landing</h1>
-  </div>
-);
-export default Landing;
