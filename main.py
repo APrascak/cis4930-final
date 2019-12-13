ff92a1951fad35b8a8eb75f097bf435f32693b5f
rahul-rudra71 <42813401+rahul-rudra71@users.noreply.github.com>
2019-12-12 23:02:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import requests
 import json
 from flask import Flask
 from firebase import firebase
-from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
 DBConn = firebase.FirebaseApplication('https://firestore-demo-3ebe9.firebaseio.com/', None)
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 def getPriceFromTradier():
     try:
@@ -23,22 +21,13 @@ def getPriceFromTradier():
     return lastPrice
 
 
-def userFunds(token, timeStamp, balanceChange):
-    data_to_upload = {
-        'Token': token,
-        'Time Stamp': timeStamp,
-        'balance Change': balanceChange
-    }
-    DBConn.post('/CentralLogging/UserFunds/', data_to_upload)
-
-
 def getUserHoldingsDb(userID):
     userDictionary = DBConn.get('/Central/Users/', '')
     for entry in userDictionary:
         result = DBConn.get('/Central/Users/'+entry+'/Stocks', '')
         if(DBConn.get('/Central/Users/'+entry+'/Token', '') == userID):
             return result
-    return "User does not exist"
+    return 0;
 
 
 def obsGetAvailableStocks():
@@ -54,13 +43,13 @@ def home():
 
 @app.route("/getStockPrice",)
 def getStockPrice():
-    result = {"stock_price ": getPriceFromTradier()}
+    result = {"stock_price": getPriceFromTradier()}
     return result
 
 
-@app.route("/getUserHoldings/<int:userID>",)
+@app.route("/getUserHoldings/<string:userID>",)
 def getUser(userID):
-    result = {"stock_amnt ": getUserHoldingsDb(userID)}
+    result = {"shares": getUserHoldingsDb(userID)}
     return result
 
 
@@ -70,64 +59,69 @@ def getOBS():
     return result
 
 
-@app.route('/buyStock/<int:token>/<int:amount>',)
+@app.route('/buy/<string:token>/<int:amount>',)
 def buyStock(token, amount):
-    # replace with api call
-    getStockPrice = getPriceFromTradier()
-    totalPrice = amount * getStockPrice
     userDictionary = DBConn.get('/Central/Users/', '')
+    tell = 0
+    userCurrentAmount = 0
+    bankCurrentAmount = 0
     # Updating Cental User Database - Users
     for entry in userDictionary:
         if(DBConn.get('/Central/Users/'+entry+'/Token', '') == token):
-            balance = DBConn.get('/Central/Users/'+entry+'/Balance', '')
-            currentAmount = DBConn.get('/Central/Users/'+entry+'/Stocks', '')
-            newBalance = balance - totalPrice
-            newAmount = currentAmount + amount
-            DBConn.put('/Central/Users/'+entry, 'Balance', newBalance)
+            userCurrentAmount = DBConn.get('/Central/Users/'+entry+'/Stocks', '')
+            newAmount = userCurrentAmount + amount
+            userCurrentAmount = newAmount
             DBConn.put('/Central/Users/'+entry, 'Stocks', newAmount)
-            result = DBConn.get('/Central/Users/'+entry, '')
+            tell = 1
             break
+    if(tell == 0):
+        data_to_upload = {
+        'Token': token,
+        'Stocks': amount
+        }
+        test = DBConn.post('/Central/Users/', data_to_upload)
+        userCurrentAmount = DBConn.get('/Central/Users/'+test['name']+'/Stocks', '')
+        bankDictionary = DBConn.get('/Central/Bank/', '')
+        for entry in bankDictionary:
+            bankCurrentAmount = DBConn.get('/Central/Bank/'+entry+'/Stock', '')
+            newAmount = bankCurrentAmount - amount
+            bankCurrentAmount = newAmount
+            DBConn.put('/Central/Bank/'+entry, 'Stock', newAmount)
+        return {"obs_shares": bankCurrentAmount, "user_shares": userCurrentAmount}
     # Updating Central Database - Bank
     bankDictionary = DBConn.get('/Central/Bank/', '')
     for entry in bankDictionary:
-        currentAmount = DBConn.get('/Central/Bank/'+entry+'/Stock', '')
-        newAmount = currentAmount - amount
+        bankCurrentAmount = DBConn.get('/Central/Bank/'+entry+'/Stock', '')
+        newAmount = bankCurrentAmount - amount
+        bankCurrentAmount = newAmount
         DBConn.put('/Central/Bank/'+entry, 'Stock', newAmount)
-    # Updating Central Logging Database - User Funds
-    userFunds(token, current_time, totalPrice)
-    r = json.dumps(result)
-    loaded_r = json.loads(r)
-    return(loaded_r)
+    return {"obs_shares": bankCurrentAmount, "user_shares": userCurrentAmount}
 
 
-@app.route('/sellStock/<int:token>/<int:amount>',)
+@app.route('/sell/<string:token>/<int:amount>',)
 def sellStock(token, amount):
-    # replace with api call
-    getStockPrice = getPriceFromTradier()
-    totalPrice = amount * getStockPrice
     userDictionary = DBConn.get('/Central/Users/', '')
+    userCurrentAmount = 0
+    bankCurrentAmount = 0
     # Updating Cental User Database - Users
     for entry in userDictionary:
         if(DBConn.get('/Central/Users/'+entry+'/Token', '') == token):
-            balance = DBConn.get('/Central/Users/'+entry+'/Balance', '')
-            currentAmount = DBConn.get('/Central/Users/'+entry+'/Stocks', '')
-            newBalance = balance + totalPrice
-            newAmount = currentAmount - amount
-            DBConn.put('/Central/Users/'+entry, 'Balance', newBalance)
+            userCurrentAmount = DBConn.get('/Central/Users/'+entry+'/Stocks', '')
+            newAmount = userCurrentAmount - amount
+            if (newAmount < 0):
+                return "insufficient stocks"
+            userCurrentAmount = newAmount
             DBConn.put('/Central/Users/'+entry, 'Stocks', newAmount)
             result = DBConn.get('/Central/Users/'+entry, '')
             break
     # Updating Central Database - Bank
     bankDictionary = DBConn.get('/Central/Bank/', '')
     for entry in bankDictionary:
-        currentAmount = DBConn.get('/Central/Bank/'+entry+'/Stock', '')
-        newAmount = currentAmount + amount
+        bankCurrentAmount = DBConn.get('/Central/Bank/'+entry+'/Stock', '')
+        newAmount = bankCurrentAmount + amount
+        bankCurrentAmount = newAmount
         DBConn.put('/Central/Bank/'+entry, 'Stock', newAmount)
-    # Updating Central Logging Database - User Funds
-    userFunds(token, current_time, -totalPrice)
-    r = json.dumps(result)
-    loaded_r = json.loads(r)
-    return(loaded_r)
+    return {"obs_shares": bankCurrentAmount, "user_shares": userCurrentAmount}
 
 
 if(__name__) == "__main__":
